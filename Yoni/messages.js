5c0bca1b9ee5d3885cfe62cd1bdb0964d005b95c
Moni Garcilazo <123327287+MoniGarcilazo@users.noreply.github.com>
2023-10-20 18:58:33 -0600

    Add files via upload

---
+++ Yoni/messages.js
@@ -0,0 +1,68 @@
+
+export default{
+
+    es:{
+        btn: {
+            myprojects: "Mis proyectos", 
+            CV: "Mi CV", 
+            contact: "Contáctame", 
+            viewmore: "Ver más"
+        },
+        nav: {
+            about: "Sobre mí", 
+            projects: "Proyectos", 
+            habilities: "Habilidades", 
+            contact: "Contacto"
+        }, 
+        title:{
+            title1: "Diseñando tus visiones:",
+            title2: "UX & UI y diseño digital"
+        },
+        description: 'Apacionado por el diseño web y enfocado en la experiencia de usuario, co-fundadora de "Insurrekta", actualmente trabajando en la industria por más de 2 añosconsecutivos', 
+        project:{
+            title:"Mis proyectos",
+            genderfloow: "Una aplicacion de salud menstrual enfocada en la experiencia de las personas trans.", 
+            insurrekta: "Colectivo sin fines de lucro que busca ayudar a micro, pequeñas y medianas empresas del estado de Mérida, Yucatán "
+        }, 
+        habilities:{
+            UXandUI: "Diseño UX&UI",
+            description1: "Investigación de usuario y mercado, prototipado y mequetación de página web.",
+            htmlandcss: "HTML y CSS",
+            description2: "Conocimientos básicos teóricos en HTML y CSS", 
+            figma: "Figma y paquetería Adobe", 
+            description3: "Experiencia usando los softwares de Figma y toda la paqueteria de Adobe"
+        }   
+    }, 
+    en:{
+        btn: {
+            myprojects: "My projects", 
+            CV: "My CV", 
+            contact: "Contact me", 
+            viewmore: "View more"
+        },
+        nav: {
+            about: "About me", 
+            projects: "Projects", 
+            habilities: "Habilities", 
+            contact: "Contact"
+        }, 
+        title:{
+            title1: "Diseñando tus visiones:",
+            title2: "UX & UI and digital design"
+        },
+        description: 'Web design passionate, focused on user experience, and "Insurekta" co-founder. Currently working in the industry for 2 years straight', 
+        project:{
+            title:"My projects",
+            genderfloow: "Menstrual health app focused on trans people's experience.", 
+            insurrekta: "Nonprofit collective aimed to boost micro, small and medium-sized companies from Mérida, Yucatán "
+        }, 
+        habilities:{
+            UXandUI: "UX & UI design",
+            description1: "Market and user research,web-page prototyping and layout design.",
+            htmlandcss: "HTML and CSS",
+            description2: "Basic theoretical knowlegde in HTML and CSS", 
+            figma: "Figma and Adobe suite", 
+            description3: "Hands-on experience on Figma and all of Adobe's suite"
+        }   
+    }
+}
